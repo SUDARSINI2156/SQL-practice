aad3cf564d956f07e60284a94cb45ff798c4f46e
SUDARSINI  A G <162299269+SUDARSINI2156@users.noreply.github.com>
2026-09-17 11:51:27 +0530

    ✅ Find Two Non-overlapping Sub-arrays Each With Target Sum | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-09-17

--- leetcode/medium/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/solution.java
+++ leetcode/medium/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/solution.java
@@ -55,7 +55,3 @@ class Solution {
         return answer;
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
