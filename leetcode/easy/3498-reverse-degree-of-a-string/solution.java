1d24b307284b89854e8f87de795263cd068be11b
SUDARSINI  A G <162299269+SUDARSINI2156@users.noreply.github.com>
2026-09-20 13:05:20 +0530

    ✅ Reverse Degree of a String | Easy | Java | Runtime: 1 ms (100.00%) | Memory: 44.1 MB | 2026-09-20

--- leetcode/easy/3498-reverse-degree-of-a-string/solution.java
+++ leetcode/easy/3498-reverse-degree-of-a-string/solution.java
@@ -4,12 +4,10 @@ class Solution {
         
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            int stringIndex = i + 1; // Menggunakan 1-indexed
-            
-            // 'a' = 26, 'b' = 25, ..., 'z' = 1
+            int stringIndex = i + 1; 
             int reversedAlphabetPos = 26 - (ch - 'a');
             
-            // Tambahkan hasil perkalian ke total
+         
             totalDegree += reversedAlphabetPos * stringIndex;
         }
         
